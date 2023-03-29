033a48ed5beea17d749c8793e7f027219e5bcf10
Alekael <118826117+Alekael@users.noreply.github.com>
2023-03-29 20:05:56 +0200

    Walls y Jugador

--- Galactic-Crystal-Odyssey/Assets/Scripts/BreakWall.cs
+++ Galactic-Crystal-Odyssey/Assets/Scripts/BreakWall.cs
@@ -7,7 +7,7 @@ public class BreakWall : MonoBehaviour
     public Sprite texture;
     public int i = 0;
 
-    private void OnTriggerEnter2D(Collider2D col){
+    private void OnCollisionEnter2D(Collision2D col){
         if(col.gameObject.tag == "Bullet" && i >= 1){
             Destroy(gameObject);
             Debug.Log("wall break");
