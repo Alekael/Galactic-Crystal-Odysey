a6a6713cf1b402f8bfcd43c77738ce5abb9f48eb
jalonjester <eliotmace@hotmail.com>
2023-05-11 20:58:46 +0200

    Fixed HUD when boss triggered

--- Galactic-Crystal-Odyssey/Assets/Scripts/Camera/FinalBossTrigger.cs
+++ Galactic-Crystal-Odyssey/Assets/Scripts/Camera/FinalBossTrigger.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class FinalBossTrigger : MonoBehaviour
 {
-    public GameObject camera;
+    public GameObject camara;
     public GameObject fondo;
     public AudioSource BossMusic;
-
+    public float yPos = 7f;
 
     void Start(){
     }
      private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")) {
             
-            GameObject camara = Instantiate(camera);
-            camara.GetComponent<VerticalCamera>().yAxis = 41.5f;
+            camara.transform.Translate(0f, yPos, 1f) ;
             camara.GetComponent<VerticalCamera>().statik = 1;
+            camara.GetComponent<VerticalCamera>().enabled = false;
             Destroy(fondo);
 
             BossMusic.Play();           
