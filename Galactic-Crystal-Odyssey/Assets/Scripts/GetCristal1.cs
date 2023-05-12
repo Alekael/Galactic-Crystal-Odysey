1da9b59e18e8ec48c28969298b1494df975f01ba
Alekael <118826117+Alekael@users.noreply.github.com>
2023-05-12 10:27:48 +0200

    Nivel dos  ligera modificación y Pantallas de victoria finales

--- Galactic-Crystal-Odyssey/Assets/Scripts/GetCristal1.cs
+++ Galactic-Crystal-Odyssey/Assets/Scripts/GetCristal1.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GetCristal : MonoBehaviour
+public class GetCristal1 : MonoBehaviour
 {
      private void OnCollisionEnter2D(Collision2D col){
       if(col.gameObject.tag == "Player"){
-        SceneManager.LoadScene("Victoria-cristal");
+        SceneManager.LoadScene("2Victoria-cristal");
       }
     }  
 }
